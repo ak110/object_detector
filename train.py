b1d78e3f2cc9c6e99336649d99938d832986e15b
ak11 <mark@aur.ll.to>
2017-09-27 00:03:08 +0900

    整理

--- train.py
+++ train.py
@@ -61,6 +61,7 @@ def _run(args, logger, result_dir: pathlib.Path, data_dir: pathlib.Path):
 
     # 訓練データからパラメータを適当に決める。
     od = ObjectDetector.create(len(CLASS_NAMES), y_train)
+    sklearn.externals.joblib.dump(od, str(result_dir.joinpath('model.pkl')))
     logger.debug('mean objects / image = %f', od.mean_objets)
     logger.debug('prior box size ratios = %s', str(od.pb_size_ratios))
     logger.debug('prior box aspect ratios = %s', str(od.aspect_ratios))
